fca8fc69f09f362a868b947c47859c68733ee3a1
Rogerio <rogeriobs83@gmail.com>
2026-09-01 14:02:42 +0100

    Corrige export do apiFetch

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,3 +2,7 @@ const apiBaseUrl =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:3001'
     : import.meta.env.VITE_API_URL.replace(/\/$/, '')
+
+export const apiFetch = (path, options = {}) => {
+  return fetch(`${apiBaseUrl}${path}`, { credentials: 'include', ...options })
+}
