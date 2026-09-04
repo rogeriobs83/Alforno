78758c794b26bbd8e68790bbad5f0b86f49e7d7b
Rogerio <rogeriobs83@gmail.com>
2026-09-04 11:49:55 +0100

    atualizando projeto

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,15 @@ app.use(cors({
   credentials: true,
 }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
 const validateString = (value, field, minLength, maxLength) => {
   if (typeof value !== 'string') {
     throw new Error(`${field} is required.`)
